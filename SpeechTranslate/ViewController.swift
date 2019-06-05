2e70bfed4133100f97af84c9741a49ecb93556bf
Krishna Hingu <13123029@student.uts.edu.au>
2019-06-06 08:51:57 +1000

    Initial Commit

---
+++ SpeechTranslate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechTranslate
+//
+//  Created by Krishna Hingu on 6/6/19.
+//  Copyright © 2019 Krishna Hingu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
